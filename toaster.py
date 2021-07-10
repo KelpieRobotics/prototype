5a336128ec65a6f8dc7e347c12c2d7a940e99583
DobaMuffin <dobamuffin@DobaMuffin>
2021-07-09 20:49:15 -0400

    Experimental support for servo

--- toaster.py
+++ toaster.py
@@ -1,14 +1,28 @@
 import curses
+import RPI.GPIO as GPIO
+import time
 from gpiozero import Robot
 
+
 robot = Robot(left=(17, 18), right=(23, 25))
+servoCtl = 4
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoCtl, GPIO.OUT)
+pin = GPIO.PWM(servoCtl, 50)
+pin.start(2.5)
 
 actions = {
     curses.KEY_UP:    robot.forward,
     curses.KEY_DOWN:  robot.backward,
     curses.KEY_LEFT:  robot.left,
     curses.KEY_RIGHT: robot.right,
+    curses.KEY_F6:    extend(2.5),
+    curses.KEY_F7:    extend(7.5),
 }
+
+def extend(value):
+    pin.ChangeDutyCycle(value)
 
 def main(window):
     next_key = None
@@ -30,5 +44,7 @@ def main(window):
                 next_key = window.getch()
             # KEY RELEASED
             robot.stop()
+	    pin.stop()
+	    GPIO.cleanup()
 
 curses.wrapper(main)
